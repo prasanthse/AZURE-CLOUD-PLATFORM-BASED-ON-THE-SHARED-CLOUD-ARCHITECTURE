e7611f032a344751d0f34d80302562eb7d0538f1
Prasanth <prasanth15sp@gmail.com>
2020-08-16 21:10:58 +0530

    Error occurs while clean springboot package

--- backend-templates/MySql Template/src/main/java/com/mysql/template/TemplateApplication.java
+++ backend-templates/MySql Template/src/main/java/com/mysql/template/TemplateApplication.java
@@ -4,19 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TemplateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TemplateApplication.class, args);
     }
 
-    //Create a database name springboot in your php-admin local host
-    //or change database name under src-->main-->-->resources-->application.properties according to your local host database
-
-    //Run xampp / wamp in order to run the backend
-
-    //create your models, repositories, controllers and services inside their relevant packages
-    //Please use capital letters as the first letter for your models, repositories, controllers, services and function names
-    //Please use simple letters as the first letters for all your variables for proper way of coding
 }
